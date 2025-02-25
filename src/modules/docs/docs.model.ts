e297ee6af9415d7e960281cb8929490f4353089d
Arumuga Durga [EXT] <arumuga.durga@iff.com>
2025-02-25 10:18:44 +0530

    docs management done

--- src/modules/docs/docs.model.ts
+++ src/modules/docs/docs.model.ts
@@ -1,16 +1,16 @@
-// import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+import { Table, Column, Model, DataType } from 'sequelize-typescript';
+@Table({ tableName: 'documents', schema: "public", timestamps: false })
+export class Document extends Model<Document> {
+  @Column({ primaryKey: true, autoIncrement: true })
+  id: number;
 
-// @Entity()
-// export class Document {
-//   @PrimaryGeneratedColumn()
-//   id: number;
+  @Column({type: DataType.STRING, allowNull: false})
+  filename: string;
 
-//   @Column()
-//   filename: string;
+  @Column({type: DataType.STRING, allowNull: false})
+  path: string;
 
-//   @Column()
-//   path: string;
+  @Column({ type: DataType.BOOLEAN, allowNull: false })
+  isActive: boolean;
+}
 
-//   @CreateDateColumn()
-//   createdAt: Date;
-// }
--- src/modules/docs/docs.service.ts
+++ src/modules/docs/docs.service.ts
@@ -1,39 +1,93 @@
-// import { Inject, Injectable, NotFoundException } from '@nestjs/common';
-// import { Document } from './docs.model';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import * as AWS from 'aws-sdk';
+import { Document } from './docs.model';
 
-// @Injectable()
-// export class DocumentService {
-//   constructor(@Inject(Document) private readonly documentModel: Document) {}
+@Injectable()
+export class DocumentService {
+  AWS_S3_BUCKET = 'demo-docs';
+  s3 = new AWS.S3({
+    accessKeyId: 'XXXXX',
+    secretAccessKey: 'YYYYY',
+  });
 
-//   async uploadDocument(filename: string, path: string) {
-//     return this.documentRepo.createDocument(filename, path);
-//   }
+  constructor(@Inject(Document) private readonly documentModel: Document) {}
 
-//   async getAllDocuments() {
-//     return this.documentRepo.findAll();
-//   }
+  async uploadDocument(file) {
+    const { originalname } = file;
 
-//   async getDocument(id: number) {
-//     const doc = await this.documentRepo.findOne(id);
-//     if (!doc) {
-//       throw new NotFoundException('Document not found');
-//     }
-//     return doc;
-//   }
+    return await this.s3_upload(
+      file.buffer,
+      this.AWS_S3_BUCKET,
+      originalname,
+    );
+  }
 
-//   async deleteDocument(id: number) {
-//     return this.documentRepo.delete(id);
-//   }
+  async s3_upload(file, bucket, name) {
+    const params = {
+      Bucket: bucket,
+      Key: String(name),
+      Body: file,
+      ACL: 'public-read',
+    };
 
-//   async updateDocument(id: number, filename?: string, path?: string) {
-//     const document = await this.documentRepo.findOne(id);
-//     if (!document) {
-//       throw new NotFoundException('Document not found');
-//     }
+    try {
+      const s3Response = await this.s3.upload(params).promise();
+      await this.documentModel.create({
+        filename: name,
+        path: s3Response?.Location,
+        isActive: true,
+      } as any);
+      return s3Response;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+async getAllDocuments() {
+    const files = await this.documentModel.findAll();
+    const fetchPromises = files.map(file => this.fetchFileFromS3(file.path));
+    return await Promise.all(fetchPromises);
+  }
   
-//     if (filename) document.filename = filename;
-//     if (path) document.path = path;
+  async getDocument(id: number) {
+    const file = await this.documentModel.findOne(id);
+    if (!file) {
+      throw new NotFoundException('Document not found');
+    }
+    return this.fetchFileFromS3(file.path);
+  }
   
-//     return this.documentRepo.save(document);
-//   }
-// }
+  private async fetchFileFromS3(path: string) {
+    const s3Key = `${path}`;
+    const params = {
+      Bucket: this.AWS_S3_BUCKET,
+      Key: s3Key,
+    };
+    return this.s3.getObject(params).promise();
+  }
+  async deleteDocument(id: number): Promise<void> {
+    const file = await this.documentModel.findOne(id);
+    if (!file) {
+      throw new NotFoundException('File not found');
+    }
+    const params = {
+      Bucket: this.AWS_S3_BUCKET,
+      Key: `${file.path}`,
+    };
+    await this.s3.deleteObject(params).promise();
+    await file.update({ isActive: false });
+  }
+
+
+  //   async updateDocument(id: number, filename?: string, path?: string) {
+  //     const document = await this.documentRepo.findOne(id);
+  //     if (!document) {
+  //       throw new NotFoundException('Document not found');
+  //     }
+
+  //     if (filename) document.filename = filename;
+  //     if (path) document.path = path;
+
+  //     return this.documentRepo.save(document);
+  //   }
+}
