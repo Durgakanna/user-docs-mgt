59850c42d11e95faf43eda01e82b0decd34aa808
Arumuga Durga [EXT] <arumuga.durga@iff.com>
2025-02-25 10:38:59 +0530

    docs module update

--- src/modules/docs/docs.module.ts
+++ src/modules/docs/docs.module.ts
@@ -1,12 +1,12 @@
-// import { Module } from '@nestjs/common';
-// import { Document } from './docs.model';
-// import { DocumentService } from './docs.service';
-// import { DocumentController } from './docs.controller';
-// import { SequelizeModule } from '@nestjs/sequelize';
+import { Module } from '@nestjs/common';
+import { Document } from './docs.model';
+import { DocumentService } from './docs.service';
+import { DocumentController } from './docs.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
 
-// @Module({
-//   imports: [SequelizeModule.forFeature([Document])],
-//   controllers: [DocumentController],
-//   providers: [DocumentService],
-// })
-// export class DocumentModule {}
+@Module({
+  imports: [SequelizeModule.forFeature([Document])],
+  controllers: [DocumentController],
+  providers: [DocumentService],
+})
+export class DocumentModule {}
